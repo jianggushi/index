77f442b1e5473e41cdc9f738dc6f4d53c5a348a2
miaomiao <237070612@qq.com>
2018-05-17 10:19:30 +0800

    js代码整理

--- static/index@0.1.0/js/script.js
+++ static/index@0.1.0/js/script.js
@@ -1,53 +1,59 @@
-(function($) {
-	
+(function ($) {
+
 	"use strict";
 
 	//Update Header Style and Scroll to Top
 	function headerStyle() {
-		// if($('#myheader').length){
-			var windowpos = $(window).scrollTop();
-			var siteHeader = $('.mainHeader');
-			var scrollLink = $('.scroll-to-top');
-			var logoImg = $('#headerLogo');
-			if (windowpos >= 200) {
-				logoImg.attr('src',"/static/images/logo-1.png");
-				siteHeader.addClass('fixed-nav');
-				siteHeader.addClass('header-scoll');
-				scrollLink.fadeIn(300);
-			} else {
-				logoImg.attr('src',"/static/images/logo-2.png");
-				siteHeader.removeClass('header-scoll');
-				siteHeader.removeClass('fixed-nav');
-				scrollLink.fadeOut(300);
-			}
-		// }
+		var windowpos = $(window).scrollTop();
+		var siteHeader = $('.mainHeader');
+		var scrollLink = $('.scroll-to-top');
+		var logoImg = $('#headerLogo');
+		if (windowpos >= 200) {
+			logoImg.attr('src', "/static/images/logo-1.png");
+			siteHeader.addClass('fixed-nav');
+			siteHeader.addClass('header-scoll');
+			scrollLink.fadeIn(300);
+		} else {
+			logoImg.attr('src', "/static/images/logo-2.png");
+			siteHeader.removeClass('header-scoll');
+			siteHeader.removeClass('fixed-nav');
+			scrollLink.fadeOut(300);
+		}
 	}
-	
+
 	headerStyle();
-	
-	
+	// Scroll to a Specific Div
+	$(".scroll-to-target").on('click', function () {
+		var target = $(this).attr('data-target');
+		// animate
+		$('html, body').animate({
+			scrollTop: $(target).offset().top
+		}, 1500);
+
+	});
+
 	// Elements Animation
-	if($('.wow').length){
+	if ($('.wow').length) {
 		var wow = new WOW(
-		  {
-			boxClass:     'wow',      // animated element css class (default is wow)
-			animateClass: 'animated', // animation css class (default is animated)
-			offset:       0,          // distance to the element when triggering the animation (default is 0)
-			mobile:       false,       // trigger animations on mobile devices (default is true)
-			live:         true       // act on asynchronously loaded content (default is true)
-		  }
+			{
+				boxClass: 'wow',      // animated element css class (default is wow)
+				animateClass: 'animated', // animation css class (default is animated)
+				offset: 0,          // distance to the element when triggering the animation (default is 0)
+				mobile: false,       // trigger animations on mobile devices (default is true)
+				live: true       // act on asynchronously loaded content (default is true)
+			}
 		);
 		wow.init();
 	}
 
 
-/* ==========================================================================
-   When document is Scrollig, do
-   ========================================================================== */
-	
-	$(window).on('scroll', function() {
+	/* ==========================================================================
+	   When document is Scrollig, do
+	   ========================================================================== */
+
+	$(window).on('scroll', function () {
 		headerStyle();
 	});
-	
+
 
 })(window.jQuery);
