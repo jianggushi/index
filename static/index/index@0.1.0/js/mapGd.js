f4e3110b8fba9cddca84a847db535929ace2fc11
miaomiao <237070612@qq.com>
2018-12-04 17:20:55 +0800

    更新高德地图

--- static/index/index@0.1.0/js/mapGd.js
+++ static/index/index@0.1.0/js/mapGd.js
@@ -47,7 +47,7 @@ updateMarker();
 
 var marker3, map = new AMap.Map('containerBJ', {
     zoom: 15,
-    center: [116.439293, 39.919796]
+    center: [116.3066400, 39.9836200]
 });
 // 实例化点标记
 function addMarker2() {
@@ -56,7 +56,7 @@ function addMarker2() {
     }
     marker3 = new AMap.Marker({
         icon: "https://webapi.amap.com/theme/v1.3/markers/n/mark_b.png",
-        position: [116.436101, 39.919656] 
+        position: [116.3066400, 39.9836200] 
     });
 
     marker3.setMap(map);
@@ -75,10 +75,10 @@ function updateMarker2() {
     // 点标记中的文本
     var markerSpan = document.createElement("span");
     markerSpan.className = 'marker';
-    markerSpan.innerHTML = "北京市朝阳区兆泰国际中心C座3层";
+    markerSpan.innerHTML = "北京市海淀区中关村创业大街2号楼3层";
     markerContent.appendChild(markerSpan);
 
     marker3.setContent(markerContent); //更新点标记内容
-    marker3.setPosition([116.436101, 39.919656]); //更新点标记位置
+    marker3.setPosition([116.3066400, 39.9836200]); //更新点标记位置
 }
 updateMarker2();
